8575ea6c50a42bd8250afba39e874e05445b4191
ritik-apurwa <ritikapurwa3500@gmail.com>
2024-08-16 12:10:12 +0530

    fix-1

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -5,14 +5,14 @@ import "../style/index.css";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
-import {HashRouter as BrowserRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey="pk_test_ZGV2b3RlZC1nb3JpbGxhLTE2LmNsZXJrLmFjY291bnRzLmRldiQ">
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <BrowserRouter>
+      <BrowserRouter basename="/subdirectory">
           <App />
         </BrowserRouter>
       </ConvexProviderWithClerk>
